fc26ed1b2008e59307df56bb6daa1837d814f008
NDK <80080316+nguyenkhanh-k2@users.noreply.github.com>
2022-10-10 13:51:35 +0000

    v1.3.4

--- src/Premium.js
+++ src/Premium.js
@@ -4,27 +4,20 @@ var { join } = require('path')
 var fs = require('fs')
 
 
-module.exports = function (defaultFuncs, api, ctx) {
-    return function(Name, args){
+module.exports = function(defaultFuncs, api, ctx) {
+    return function(Name, args) {
         var Method = {}
-        fs.readdirSync(join(__dirname, "../Func")).filter((/** @type {string} */File) => File.endsWith(".js") && !File.includes('Dev_')).map((/** @type {string} */File) => Method[File.split('.').slice(0, -1).join('.')] = require(`../Func/${File}`)(defaultFuncs, api, ctx));
+        fs.readdirSync(join(__dirname, "../Func")).filter(( /** @type {string} */ File) => File.endsWith(".js") && !File.includes('Dev_')).map(( /** @type {string} */ File) => Method[File.split('.').slice(0, -1).join('.')] = require(`../Func/${File}`)(defaultFuncs, api, ctx));
         if (Method[Name] == undefined) {
             return (`Method ${Name} not found`);
-        }
-        else {
+        } else {
             try {
-                if (process.env.HalzionVersion == 1973 && global.Fca.Data.PremText.includes("Premium")) {
-                    return Method[Name](args).then((/** @type {string} */Data) => {
-                        return Data;
-                    })
-                }
-                else {
-                    return ("Mua Premium Đi Rồi Sài Ông Cháu Ơi !!");
-                }
-            }
-            catch (e) {
+                return Method[Name](args).then(( /** @type {string} */ Data) => {
+                    return Data;
+                })
+            } catch (e) {
                 console.log(e);
             }
         }
-    }    
+    }
 };
